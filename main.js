b2c0f19d2ee832b579e3d107b2c0b9789bd5ceac
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-08 07:38:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Implemented different scenarios to experience increment operators.
+var age = 21;
+--age;
+--age;
+--age;
+--age;
+--age;
+console.log(age); //16
---
+++ main.ts
@@ -0,0 +1,8 @@
+//Implemented different scenarios to experience increment operators.
+let age=21;
+--age;
+--age;
+--age;
+--age;
+--age;
+console.log(age); //16
